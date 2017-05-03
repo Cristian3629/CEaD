ea2c342a98bda7716d8d3891d2c999fec8416d8f
cristian3629 <cristian3629@gmail.com>
2017-05-03 11:40:26 -0300

    Agrego formas de seleccion

--- Embedded subtitles/Exercises JQuery/E1/E1.js
+++ Embedded subtitles/Exercises JQuery/E1/E1.js
@@ -7,3 +7,12 @@ $(document).ready(function(){
      .css("border", "2px solid red");
    });
 });
+
+
+//$(this).hide() - hides the current element.
+
+//$("p").hide() - hides all <p> elements.
+
+//$(".test").hide() - hides all elements with class="test".
+
+//$("#test").hide() - hides the element with id="test".
