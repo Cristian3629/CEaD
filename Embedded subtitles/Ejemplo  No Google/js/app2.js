c0413f685a9cf514d9e807b98b1b532b10139f90
cristian3629 <cristian3629@gmail.com>
2017-05-12 16:45:56 -0300

    ya se puede obtener el id de los caption de un video

--- Embedded subtitles/Ejemplo  No Google/js/app2.js
+++ Embedded subtitles/Ejemplo  No Google/js/app2.js
@@ -1,11 +1,17 @@
+var apiKey = "AIzaSyCswdcyFNVbzBjApp5MjBCpNkxPpCrgAhs";
+var clientId = "1052872841813-nrdvdimngi0vqh8r9nbvf9ojmbnp60bv.apps.googleusercontent.com"
+var scope = "https://www.googleapis.com/auth/youtube.force-ssl";
+
+
+
 $(function(){
   $("form").on('submit',function(event) {
     event.preventDefault();
     //Solicito los ids de los captions del id del video que pasé
     var request = gapi.client.youtube.captions.list({
-      part: "snippet",
-      //id del video
-      videoId: "PRU2ShMzQRg",
+      part: "id",
+      //id del video que me interesa
+      videoId: "QdjO0e10O_I",
       //solicito solo el campo de id del caption
       fields : "items/id"
     });
@@ -15,6 +21,7 @@ $(function(){
       //var responseJSON = JSON.stringify(result);
       //ver como acceder a JSON
       //var id = respondeJSON["items"]:"id";
+      //muestro el resultado
       console.log(result);
       var responseJSON = JSON.stringify(result);
       //console.log(responseJSON["items"]);
@@ -29,8 +36,9 @@ $(function(){
 
 function apiYoutubeReady() {
   gapi.client.init({
-    apiKey: "AIzaSyCswdcyFNVbzBjApp5MjBCpNkxPpCrgAhs",
-    clientId: "1052872841813-nrdvdimngi0vqh8r9nbvf9ojmbnp60bv.apps.googleusercontent.com"
+    apiKey: apiKey,
+    clientId: clientId,
+    scope: scope
   });
   gapi.client.load("youtube", "v3", function() {
         // yt api is ready
