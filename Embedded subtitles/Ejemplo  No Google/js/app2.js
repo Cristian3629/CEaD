76fd38257add2cd6a18c6f5e1c2bd1c1ea829de0
cristian3629 <cristian3629@gmail.com>
2017-05-12 18:59:05 -0300

    falta resolver el loggeo con oauth2

--- Embedded subtitles/Ejemplo  No Google/js/app2.js
+++ Embedded subtitles/Ejemplo  No Google/js/app2.js
@@ -1,10 +1,24 @@
 var apiKey = "AIzaSyCswdcyFNVbzBjApp5MjBCpNkxPpCrgAhs";
-var clientId = "1052872841813-nrdvdimngi0vqh8r9nbvf9ojmbnp60bv.apps.googleusercontent.com"
-var scope = "https://www.googleapis.com/auth/youtube.force-ssl";
+var clientId = "223976978127-egnpt8tu7bf32cqf2h2m7qor7lbe20b6.apps.googleusercontent.com"
+var scope = 'https://www.googleapis.com/auth/youtubepartner';
+
+
+var GoogleAuth;
+
+function getCaption(idCaption){
+  var request = gapi.client.youtube.captions.download({
+    id: idCaption,
+    tfmt: "vtt"
+  });
+
+  request.execute(function(){
+    console.log("response is successful");
+  });
+}
 
 
 
-$(function(){
+function makeRequest(){
   $("form").on('submit',function(event) {
     event.preventDefault();
     //Solicito los ids de los captions del id del video que pasé
@@ -17,30 +31,53 @@ $(function(){
     });
 
     request.execute(function(response){
-      var result= response.result;
-      //var responseJSON = JSON.stringify(result);
-      //ver como acceder a JSON
-      //var id = respondeJSON["items"]:"id";
-      //muestro el resultado
-      console.log(result);
-      var responseJSON = JSON.stringify(result);
-      //console.log(responseJSON["items"]);
+      //var result = response.result;
+      var idCaption = response.items[0].id;
+      console.log("IdCaption:"+idCaption);
+      getCaption(idCaption);
     });
   });
-})
+}
+
+var isAuthorized;
+var currentApiRequest;
 
 
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    isAuthorized = true;
+    if (currentApiRequest) {
+      sendAuthorizedApiRequest(currentApiRequest);
+    }
+  } else {
+    isAuthorized = false;
+  }
+}
 
 
+function sendAuthorizedApiRequest(requestDetails) {
+  currentApiRequest = requestDetails;
+  if (isAuthorized) {
+    // Make API request
+    // gapi.client.request(requestDetails)
+
+    // Reset currentApiRequest variable.
+    makeRequest();
+  } else {
+    GoogleAuth.signIn();
+  }
+}
 
 
-function apiYoutubeReady() {
+function apiYoutubeReady(){
   gapi.client.init({
     apiKey: apiKey,
     clientId: clientId,
     scope: scope
   });
-  gapi.client.load("youtube", "v3", function() {
-        // yt api is ready
-    });
+  //.then(function(){
+    //GoogleAuth = gapi.auth2.getAuthInstance();
+    //GoogleAuth.isSignedIn.listen(updateSigninStatus);
+  //});
+  gapi.client.load("youtube", "v3", makeRequest);
 }
