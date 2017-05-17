6ae92d438fc4061a810b495ae7630428862101a6
cristian3629 <cristian3629@gmail.com>
2017-05-17 15:59:29 -0300

    ya se empieza a trabajar con la API IFrame de Youtube

---
+++ Embedded subtitles/playerYoutube.js
@@ -0,0 +1,39 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+var player;
+function onYouTubeIframeAPIReady(){
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: videoId,
+    events: {
+      //dado los eventos, indico a que funciones llamo. aca deberia hacer dado
+      //que se esta reproduciendo, llamar a una funcion mia.
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+    });
+}
+
+//Si ya se buffereo todo, lo reproduzco
+function onPlayerReady(event){
+        event.target.playVideo();
+}
+
+
+var done = false;
+//Dado que esta reproduciendo
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo() {
+        player.stopVideo();
+}
